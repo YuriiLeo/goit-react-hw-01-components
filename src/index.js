accd72d580422853fdd229751de5704157badc8f
YuriiLeo <yuriileontenko@gmail.com>
2022-09-12 00:19:17 +0300

    add new files

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-const element = React.createElement("div", { a: 5, b: 10, children: "Hello world" });
-console.log(element);
-
-ReactDOM.render(element, document.querySelector("#root"));
+const element = React.createElement('div', {
+  a: 5,
+  b: 10,
+  children: 'Hello world',
+});
